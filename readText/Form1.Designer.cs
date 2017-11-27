3f086c8e79ded5ae34747771af134fc4f668b085
HongChinhh <htrinhpadu@gmail.com>
2017-11-27 10:52:52 +0700

    1
    .

---
+++ readText/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace readText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.cboPath = new System.Windows.Forms.ComboBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lstFoodCalculator = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(264, 29);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(121, 21);
+            this.cboSheet.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sheet:";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 32);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "&Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // cboPath
+            // 
+            this.cboPath.FormattingEnabled = true;
+            this.cboPath.Location = new System.Drawing.Point(93, 32);
+            this.cboPath.Name = "cboPath";
+            this.cboPath.Size = new System.Drawing.Size(121, 21);
+            this.cboPath.TabIndex = 4;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(404, 27);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lstFoodCalculator
+            // 
+            this.lstFoodCalculator.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFoodCalculator.FullRowSelect = true;
+            this.lstFoodCalculator.GridLines = true;
+            this.lstFoodCalculator.Location = new System.Drawing.Point(12, 62);
+            this.lstFoodCalculator.Name = "lstFoodCalculator";
+            this.lstFoodCalculator.Size = new System.Drawing.Size(572, 389);
+            this.lstFoodCalculator.TabIndex = 6;
+            this.lstFoodCalculator.UseCompatibleStateImageBehavior = false;
+            this.lstFoodCalculator.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 463);
+            this.Controls.Add(this.lstFoodCalculator);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.cboPath);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSheet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ComboBox cboPath;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ListView lstFoodCalculator;
+    }
+}
+
---
+++ readText/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace readText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // tao doi tuong mo tep tin
+            OpenFileDialog fopen = new OpenFileDialog();
+            //chi ra duoi
+            fopen.Filter = "(tat ca cac tep)|*.*|(Cac tep excel)|*.xlsx";
+            fopen.ShowDialog();
+            if(fopen.FileName !="")
+            {
+                cboPath.Text = fopen.FileName;
+                // tao doi tuong
+                Excel.Application app = new Excel.Application();
+                Excel.Workbook wb = app.Workbooks.Open(fopen.FileName);
+                try
+                {
+                    // mo sheet
+                    Excel._Worksheet sheet = wb.Sheets[1];
+                    Excel.Range range = sheet.UsedRange;
+                    // doc du lieu
+                    int rows = range.Rows.Count;
+                    int cols = range.Columns.Count;
+                    // doc dong tieu de de tao cot cho ListView
+                    for(int c =1;c<=cols; c++)
+                    {
+                        string columname = range.Cells[1, c].Value.ToString();
+                        ColumnHeader col = new ColumnHeader();
+                        col.Text = columname;
+                        col.Width = 120;
+                        lstFoodCalculator.Columns.Add(col);
+                    }
+                    // doc du lieu
+                    for(int i=2;i<=rows;i++)
+                    {
+                        ListViewItem item =  new ListViewItem();
+                        for(int j=1;j<cols;j++)
+                        {
+                            if (j == 1)
+                                item.Text = range.Cells[i, j].Value.ToString();
+                            else
+                                item.SubItems.Add(range.Cells[i, j].Value.ToString());
+                        }
+                        lstFoodCalculator.Items.Add(item);  
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ban khong chon tep tin nao", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog fsave = new SaveFileDialog();
+            fsave.Filter = "(tat ca cac tep)|*.*|(Cac tep excel)|*.xlsx";
+            fsave.ShowDialog();
+            if (fsave.FileName != "")
+            {//tao fie exce
+                Excel.Application app = new Excel.Application();
+                    // tao workbook
+                Excel.Workbook wb = app.Workbooks.Add(Type.Missing);
+                // tao shet 
+
+            }
+
+                    else
+            {
+                MessageBox.Show("Ban khong chon tep tin nao", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
